9ee77533078a96721e03888ff1eb5169251c134f
Grzegorz Górnisiewicz <contact@longroad.co>
2016-12-19 16:38:55 +0100

    Initial Commit

---
+++ HubchatPostLister/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HubchatPostLister
+//
+//  Created by Grzegorz Górnisiewicz on 19.12.2016.
+//  Copyright © 2016 Long Road. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
